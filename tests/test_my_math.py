67b17f143db6fadbc9b88bbe041a170f0f4f51c3
olivier mucyo <mucyo@andrews.edu>
2025-02-09 09:42:41 -0500

    add files

--- tests/test_my_math.py
+++ tests/test_my_math.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.my_math import sum, multiply, difference
+from src.my_math import sum, multiply, difference,absolute_sum
 
 def test_sum():
     res = sum(1,1)
@@ -8,16 +8,34 @@ def test_sum():
 
 # Work together
 ## Test multiply
+def test_multiply():
+    res = multiply(2,2)
+    assert res == 4
+    
 
 
 # Check for understanding
 ## Test difference
+def test_difference():
+    res = difference(3,1)
+    assert res == 2
 
 
 # Work together
 ## Test absolute sum
+def test_it_should_return_positive_numbers_():
+    res = absolute_sum(1,2)
+    assert res == 3
+
+def test_it_should_return_positive_numbers_2():
+    res = absolute_sum(-1,2)
+    assert res == 3
 
 
 
 # Check for understanding
 ## Test calculate age
+
+def calc_age_1():
+   res = birth_year(2025, 40, false)
+   assert res ==1984 
